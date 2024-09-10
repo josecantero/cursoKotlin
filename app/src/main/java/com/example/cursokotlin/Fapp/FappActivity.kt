57b21571575118ace0405cbc26a4bd2bf65908d9
josecantero <josedejesuscantero@gmail.com>
2024-09-10 12:06:37 -0400

    se agregó funcionalidad a la pantalla principal para agregar un texto y mandarlo a una pantalla de saludo

--- app/src/main/java/com/example/cursokotlin/Fapp/FappActivity.kt
+++ app/src/main/java/com/example/cursokotlin/Fapp/FappActivity.kt
@@ -1,8 +1,12 @@
 package com.example.cursokotlin.Fapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cursokotlin.R
@@ -13,6 +17,19 @@ class FappActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_fapp)
+        val editText = findViewById<AppCompatEditText>(R.id.editText)
+        val btnPulsar = findViewById<AppCompatButton>(R.id.btnPulsar)
+
+
+        btnPulsar.setOnClickListener{
+            val nameText = editText.text.toString()
+            if(nameText.isNotEmpty()){
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("extraName", nameText)
+                startActivity(intent)
+            }
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/cursokotlin/Fapp/ResultActivity.kt
@@ -0,0 +1,29 @@
+package com.example.cursokotlin.Fapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cursokotlin.R
+
+class ResultActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+
+        val nameResult = findViewById<TextView>(R.id.nameResult)
+        val name:String = intent.extras?.getString("extraName").orEmpty()
+        nameResult.text = "Hola $name"
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
