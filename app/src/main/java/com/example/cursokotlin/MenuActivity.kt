c4299ada12d53977c8be40cbc3616ca04ccb552c
josecantero <josedejesuscantero@gmail.com>
2024-09-14 21:53:55 -0400

    cambios de privacidad de la función gotosaludapp

--- app/src/main/java/com/example/cursokotlin/MenuActivity.kt
+++ app/src/main/java/com/example/cursokotlin/MenuActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cursokotlin.Fapp.FappActivity
-import com.example.cursokotlin.Fapp.ResultActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +26,7 @@ class MenuActivity : AppCompatActivity() {
         }
     }
 
-    fun navigateToSaludapp(){
+    private fun navigateToSaludapp(){
         val intent = Intent(this,  FappActivity::class.java)
         startActivity(intent)
     }
