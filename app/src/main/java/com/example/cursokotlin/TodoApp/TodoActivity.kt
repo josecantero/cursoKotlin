487a658183a1fe2faf46c4ddcb9eda4349c5c6bf
josecantero <josedejesuscantero@gmail.com>
2024-09-24 15:14:37 -0400

    se agregaron los radio buttons y el botón para crear nuevas tareas, además la funcionalidad que ya crea nuevas tareas.

--- app/src/main/java/com/example/cursokotlin/TodoApp/TodoActivity.kt
+++ app/src/main/java/com/example/cursokotlin/TodoApp/TodoActivity.kt
@@ -1,8 +1,14 @@
 package com.example.cursokotlin.TodoApp
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.cursokotlin.R
 import com.example.cursokotlin.TodoApp.TaskCategory.*
 import com.example.cursokotlin.TodoApp.TaskAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
 
@@ -25,10 +32,13 @@ class TodoActivity : AppCompatActivity() {
         Task("PreubaOtro", other)
     )
 
+    // COMPONENTS
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
     private lateinit var rvTasks:RecyclerView
     private lateinit var taskAdapter:TaskAdapter
+    private lateinit var btnTodoAddTask: FloatingActionButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +47,7 @@ class TodoActivity : AppCompatActivity() {
 
         initComponents()
         initUI()
+        initListeners()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -48,6 +59,39 @@ class TodoActivity : AppCompatActivity() {
     private fun initComponents(){
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        btnTodoAddTask = findViewById(R.id.btnTodoAddTask)
+
+    }
+
+    private fun initListeners(){
+        btnTodoAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
+        val btnDialogAddTask: AppCompatButton = dialog.findViewById(R.id.btnDialogAddTask)
+        val etTask:EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories:RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnDialogAddTask.setOnClickListener {
+            val rbSelected = rgCategories.checkedRadioButtonId
+            val selectedRadioButtom:RadioButton = rgCategories.findViewById(rbSelected)
+            val currentCategory:TaskCategory = when(selectedRadioButtom.text){
+                "Negocios" -> Bussines
+                "Personal" -> personal
+                else -> other
+            }
+            tasks.add(Task(etTask.text.toString(), currentCategory))
+            updateTask()
+            dialog.hide()
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun updateTask(){
+        taskAdapter.notifyDataSetChanged()
     }
 
     private fun initUI(){
